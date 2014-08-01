d956f5a3f8326ce62bf4bef205e0b0a9e7f54e07
Ciro S. Costa <ciro.costa@usp.br>
2014-08-01 10:34:34 -0300

    f commit

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp');
+var livereload = require('gulp-livereload');
+
+gulp.task('default', function () {
+	livereload.listen();
+	gulp
+		.watch('src/**')
+		.on('change', livereload.changed);
+}
