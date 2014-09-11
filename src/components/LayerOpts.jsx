2c6753ba472e9b40e769583abc9b79a13f48b258
Ciro S. Costa <ciro.costa@usp.br>
2014-09-11 18:26:34 -0300

    removes dist, updates opts and layeropts

--- src/components/LayerOpts.jsx
+++ src/components/LayerOpts.jsx
@@ -5,99 +5,80 @@
 'use strict';
 
 var React = require('react');
+var clone = (obj) => JSON.parse(JSON.stringify(obj));
 var _ = require('underscore');
 
-var clone = (obj) => JSON.parse(JSON.stringify(obj));
+var STRING_OR_NUMBER = React.PropTypes.oneOfType([
+  React.PropTypes.string,
+  React.PropTypes.number
+]);
 
 var LayerOpts = React.createClass({
   propTypes: {
-    onLayerChanges: React.PropTypes.func
+    width: STRING_OR_NUMBER,
+    height: STRING_OR_NUMBER,
+    thickness: STRING_OR_NUMBER,
+    onLayerChange: React.PropTypes.func
   },
 
-  EMPTY_LAYER: {
-    width: '',
-    height: '',
-    thickness: ''
+  getDefaultProps () {
+    return {
+      width: '',
+      height: '',
+      thickness: ''
+    };
   },
 
   getInitialState () {
     return {
-      layers: [clone(this.EMPTY_LAYER)]
+      layer: {
+        width: this.props.width,
+        height: this.props.height,
+        thickness: this.props.thickness
+      }
     };
   },
 
-  componentWillUpdate () {
-    if (this.props.onLayerChanges)
-      this.props.onLayerChanges(this.state.layers);
-  },
+  notifyLayerChange: _.debounce(() => {
+    if (this.props.onLayerChange)
+      this.props.onLayerChange(this.state.layer);
+  }, 300),
 
   handleChange (e) {
-    var id = e.target.dataset.id;
     var name = e.target.dataset.name;
-    var newLayers = this.state.layers.slice();
+    var newLayer = clone(this.state.layer);
 
-    newLayers[id][name] = e.target.value;
-
-    this.setState({
-      layers: newLayers
-    });
-  },
-
-  handleClick (e) {
-    var newLayers;
-
-    switch (e.target.dataset.type) {
-      case 'add':
-        newLayers = this.state.layers.concat(clone(this.EMPTY_LAYER));
-        break;
-      case 'remove':
-        newLayers = this.state.layers.slice();
-        newLayers.splice(e.target.dataset.id, 1);
-        break;
-    }
+    newLayer[name] = e.target.value;
 
     this.setState({
-      layers: newLayers
+      layer: newLayer
     });
+
+    this.notifyLayerChange();
   },
 
   render () {
-    var layers = _.map(this.state.layers, (layer, i) =>
-      <li key={i}>
-        <p>
-          Layer <button data-id={i}
-                        data-type="remove"
-                        onClick={this.handleClick}>remove</button>
-        </p>
+    return (
+      <div>
         <label>Width</label>
         <input type="text"
                data-name="width"
-               data-id={i}
                onChange={this.handleChange}
-               value={layer.width}
+               value={this.state.layer.width}
                placeholder="how long" />
         <label>Height</label>
         <input type="text"
                data-name="height"
-               data-id={i}
                onChange={this.handleChange}
-               value={layer.height}
+               value={this.state.layer.height}
                placeholder="how high it is" />
         <label>Thickness</label>
         <input type="text"
                data-name="thickness"
-               data-id={i}
                onChange={this.handleChange}
-               value={layer.thickness}
+               value={this.state.layer.thickness}
                placeholder="how thick" />
-        <button data-type="add" onClick={this.handleClick}>New Layer</button>
-      </li>
-    );
-
-    return (
-      <ol>
-        {layers}
-      </ol>
+      </div>
     );
   }
 });
--- src/components/Options.jsx
+++ src/components/Options.jsx
@@ -7,44 +7,84 @@
 var React = require('react');
 var _ = require('underscore');
 var LayerOpts = require('./LayerOpts.jsx');
+var clone = (obj) => JSON.parse(JSON.stringify(obj));
 
 var Options = React.createClass({
-  handleChange (e) {
-    switch (e.target.dataset.name) {
-      case 'width':
-
-        break;
-
-      case 'height':
-
-        break;
-
-      case 'precision':
-
-        break;
-
-      case 'layers':
-
-        break;
-    }
+  getInitialState () {
+    return {
+      width: "",
+      height: "",
+      precision: "",
+      layers: [1]
+    };
   },
 
-  handleLayerChanges (layers) {
+  handleChange (e) {
+    var newState = clone(this.state);
+    newState[e.target.dataset.name] = e.target.value;
+
+    this.setState(newState);
+  },
+
+  handleLayerChange (layer) {
+    console.log(layer);
+  },
+
+  handleClick (e) {
+    var newLayers = this.state.layers.slice();
+
+    switch (e.target.dataset.type) {
+      case 'add':
+        newLayers.push({
+          width: '',
+          height: '',
+          thickness: ''
+        });
+        break;
+      case 'remove':
+        newLayers.splice(e.target.dataset.id, 1);
+        break;
+    }
+
     this.setState({
-      layers: layers.length
+      layers: newLayers
     });
   },
 
   render () {
+    var layers = _.map(this.state.layers, (layer, i) =>
+      <li key={i}>
+        <button onClick={this.handleClick}
+                data-id={i}
+                data-type="remove">
+          Remove Layer
+        </button>
+        <LayerOpts onLayerChange={this.handleLayerChange}
+                   width={layer.width}
+                   height={layer.height}
+                   thickness={layer.thickness} />
+      </li>
+    );
+
     return (
       <div className="Options">
         <label>Width</label>
-        <input onChange={this.handleChange} data-name="width" />
+        <input onChange={this.handleChange}
+               data-name="width"
+               value={this.state.width} />
         <label>Height</label>
-        <input onChange={this.handleChange} data-name="height" />
+        <input onChange={this.handleChange}
+               data-name="height"
+               value={this.state.height} />
         <label>Precision</label>
-        <input onChange={this.handleChange} data-name="precision" />
-        <LayerOpts onLayerChanges={this.handleLayerChanges}/>
+        <input onChange={this.handleChange}
+               data-name="precision"
+               value={this.state.precision} />
+        <p>Layers: {this.state.layers.length}</p>
+        <button onClick={this.handleClick} data-type="add">Add layers</button>
+        <ol>
+          {layers}
+        </ol>
       </div>
     );
   }
